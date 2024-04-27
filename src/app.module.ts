ffe5bf3b506bb1cb64f12da980ae3caa6d7b6715
Mat <mt.ribeiro4@gmail.com>
2024-04-27 14:24:52 -0300

    feat: removing app.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppService } from '../test/app.service';
 import { WeekdaysModule } from './weekdays/weekdays.module';
 
 @Module({
   imports: [WeekdaysModule],
   controllers: [],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
